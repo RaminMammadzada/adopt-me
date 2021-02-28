61939f953d644b3af900cdcc97bc6a514b7a5e72
Ramin Mammadzada <remki-design@hotmail.com>
2021-02-28 19:08:04 +0300

    Add component for parameter, custom hook for dropdown and useEffect hook to fetch data when needed.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from "react";
 import render from "react-dom";
-import Pet from "./Pet";
+
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  // return (
-  //   <div>
-  //     <h1>Adopt Me!</h1>
-  //     <Pet name="Luna" animal="dog" breed="Havanese" />
-  //     <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-  //     <Pet name="Doink" animal="cat" breed="Mix" />
-  //   </div>
-  // );
 
   return (
     <div>
-      
+
       <h1 id="something-important">Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bird" breed="Coctaliel" />
-      <Pet name="Doink" animal="Cat" breed="Mixed" />
+      <SearchParams />
     </div>
   )
 
